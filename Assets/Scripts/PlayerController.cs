c810adaaf9aa1b4c90825669bc30ca8b758bd72d
laurazp <lzafrap@uoc.edu>
2021-11-18 22:25:05 +0000

    Nivel 3 actualizado, añadidos assets, creada nave y controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, yMin, yMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Boundary boundary;
+
+    private Rigidbody rig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0f);
+        rig.velocity = movement * speed;
+        rig.position = new Vector3(Mathf.Clamp(rig.position.x, boundary.xMin, boundary.xMax), Mathf.Clamp(rig.position.y, boundary.yMin, boundary.yMax), 0f);
+        rig.rotation = Quaternion.Euler(90, 180, rig.velocity.x);
+    }
+}
