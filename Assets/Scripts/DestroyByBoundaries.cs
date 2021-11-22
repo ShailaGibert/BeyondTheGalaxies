8a4124bedebfded7515299f1599dbe14498bbf48
laurazp <lzafrap@uoc.edu>
2021-11-22 14:01:30 +0000

    Actualizado nivel 3, añadida nave enemiga y scripts destruir objetos

---
+++ Assets/Scripts/DestroyByBoundaries.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByBoundaries : MonoBehaviour
+{
+    void  OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+
+}
---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Boundary")) return;
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
