63442a4bb3f3fc5cc001bc73aff37f163b8fd77e
Jordi <jpodeschva@uoc.edu>
2021-11-29 23:03:55 +0100

    cambio camera y luces ok level 2

--- Assets/Scripts/RandomRotator.cs
+++ Assets/Scripts/RandomRotator.cs
@@ -2,26 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
-
+public class RandomRotator : MonoBehaviour
+{
+    public float tumble;
     private Rigidbody rig;
-
-
-    void Awake() {
+    
+    void Awake()
+    {
 
         rig = GetComponent<Rigidbody>();
-    
+
     }
 
 
 
     // Start is called before the first frame update
-    void Start() {
+    void Start()
+    {
 
-        Vector3 angularVelocity = new Vector3(Random.Range(-1,1), Random.Range(-1, 1), Random.Range(-1, 1)).normalized;
-        rig.angularVelocity = angularVelocity;
-        
+        //Vector3 angularVelocity = new Vector3(Random.Range(-1, 1), Random.Range(-1, 1), Random.Range(-1, 1)).normalized;
+
+        //Vector3 angularVelocity = Random.insideUnitSphere;
+        //rig.angularVelocity = angularVelocity * tumble;
+        rig.angularVelocity = Random.insideUnitSphere * tumble;
+
     }
 
- 
 }
