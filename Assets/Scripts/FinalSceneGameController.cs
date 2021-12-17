6493701c8d8db8551b28ebc0d968be72b8b84344
Jordi <jpodeschva@uoc.edu>
2021-12-17 18:42:14 +0100

    intentando xml
    intentando xml

--- Assets/Scripts/FinalSceneGameController.cs
+++ Assets/Scripts/FinalSceneGameController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Xml;
 
 public class FinalSceneGameController : MonoBehaviour
 {
@@ -28,5 +29,47 @@ public class FinalSceneGameController : MonoBehaviour
     {
         scoreText.text = "Your Score: " + score;
         //finalSceneGameController.ScoreText.text = "Score: " + score;
+    }
+
+    //NEW TODO ABAJO 
+
+    
+
+    public void SaveByXML() {
+
+        Save save = saveScore();
+
+        XmlDocument xmlDocument = new XmlDocument();
+
+        #region CreateXML elements
+
+        XmlElement root = xmlDocument.CreateElement("Save");
+        root.SetAttribute("FileName", "File_01");     
+
+        XmlElement score = xmlDocument.CreateElement("Total Score");
+        score.InnerText = save.score.ToString();
+        root.AppendChild(score);
+
+        #endregion
+
+        xmlDocument.AppendChild(root);
+        xmlDocument.Save(Application.dataPath + "/DataXML.text");
+
+            if(File.Exists(Application.dataPath + "/DataXML.text")) {
+
+            Debug.Log("XML FILE SAVED");
+        }
+    
+    }
+
+    private Save saveScore()
+    {
+
+        Save save = new Save();
+
+        save.score = score;
+
+        return save;
+
     }
 }
