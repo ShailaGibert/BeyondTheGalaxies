0f7b033f6ced23aa1ff52cdc60f577e9bafa1b56
Jordi <jpodeschva@uoc.edu>
2021-12-26 18:46:20 +0100

    introduciendo codigo ejemplo
    introduciendo codigo ejemplo

--- Assets/Tests/LevelTest.cs
+++ Assets/Tests/LevelTest.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class LevelTest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private GameObject hazard;
+    private GameObject enemyShift;
+    private GameObject enemyExplosion;
+    private GameObject playerExplosion;
+    private DestroyByContact2 destroyByContact2;
+
+
+
+    public void Setup()
     {
-        
+        /*
+        player = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Player"), new Vector3(0, 0, 0), Quaternion.identity);
+        player.GetComponent<Rigidbody>().useGravity = false;
+
+        enemy = MonoBehaviour.Instantiate(Resources.Load<GameObject>("Prefabs/Enemy"), new Vector3(200, 0, 0), Quaternion.identity);
+        enemy.GetComponent<Rigidbody>().useGravity = false;
+        */
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
+
+
 }
